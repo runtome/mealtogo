da373185097977f680be2aeae18f414b3753a50f
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-12-07 14:18:50 +0700

    Making theme

--- src/features/restaurants/components/restaurant-info.card.js
+++ src/features/restaurants/components/restaurant-info.card.js
@@ -3,6 +3,8 @@ import { Text , StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
+
+
 const RestaurantCard = styled(Card)`
   background-color: white;
 `;
@@ -14,7 +16,7 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Title = styled.Text`
   padding: 16px;
-  color: red;
+  color: ${(props) => props.theme.colors.ui.primary};
 `;
 
 
