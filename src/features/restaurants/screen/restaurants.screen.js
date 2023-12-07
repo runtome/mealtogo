85c87f280567638322bf8bbe25d59f85481488ad
SUPHOT NARAPONG <suphot.n@gmail.com>
2023-12-07 19:37:37 +0700

    theme cleanup

--- src/features/restaurants/screen/restaurants.screen.js
+++ src/features/restaurants/screen/restaurants.screen.js
@@ -12,13 +12,12 @@ const SafeArea = styled(SafeAreaView)`
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding:  ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  background-color: ${(props) => props.theme.space[3]};
 `;
 
 export const RestaurantsScreen = () => (
